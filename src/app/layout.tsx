095977803ebc7b091e072ccd5f682d32ec756a64
Gutinnho <glunardi99@gmail.com>
2023-06-06 12:24:20 -0300

    feat: Clean initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import './globals.css';
+import { Roboto } from 'next/font/google';
+
+const roboto = Roboto({
+	subsets: ['latin'],
+	weight: ['300', '500', '700'],
+});
+
+export const metadata = {
+	title: 'Barbearia Domingos Zion',
+	description:
+		'Descubra o melhor lugar para cuidar do seu estilo na nossa barbearia. Oferecemos serviços de corte de cabelo, barba terapia e hidratação capilar. Nossos experientes barbeiros garantem um atendimento personalizado, utilizando técnicas modernas e produtos de alta qualidade. Agende sua visita e desfrute de um momento de cuidado e relaxamento em um ambiente acolhedor. Venha para a Barbearia Domingos Zion e descubra o poder de um visual impecável.',
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang='pt-br'>
+			<body className={roboto.className}>{children}</body>
+		</html>
+	);
+}
+
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<main className='flex h-full w-full flex-1 items-center justify-center'>
+			<p className='text-lg text-blue-700'>Zion Barbearia</p>
+		</main>
+	);
+}
