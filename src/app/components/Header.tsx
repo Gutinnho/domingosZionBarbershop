a442147398986a3c9a3f1dc69359e478ea5c8299
Gutinnho <glunardi99@gmail.com>
2023-06-14 15:21:17 -0300

    fix(header): Fixed color and position, added color transition
    The color of the header component has been corrected, its position has been changed from absolute to fixed, and a smooth color transition has been added when scrolling

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,15 +1,17 @@
 'use client';
 import Image from 'next/image';
-import logo from '@/assets/Logo.png';
-import { List, X } from '@phosphor-icons/react';
 import { useEffect, useState } from 'react';
 
+import { List, X } from '@phosphor-icons/react';
+
+import logo from '@/assets/Logo.png';
 import FacebookIconSVG from './SVGs/FacebookIconSVG';
 import InstagramIconSVG from './SVGs/InstagramIconSVG';
 import WhatsappIcon from './SVGs/WhatsappIconSVG';
 
 function Header() {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const [isHeaderSticky, setIsHeaderSticky] = useState(false);
 
 	const toggleMenu = () => {
 		setIsMenuOpen(!isMenuOpen);
@@ -20,10 +22,23 @@ function Header() {
 		currentWidth >= 768 ? setIsMenuOpen(true) : setIsMenuOpen(false);
 	}, [setIsMenuOpen]);
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const isSticky = window.pageYOffset > 0;
+      setIsHeaderSticky(isSticky);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
 	return (
 		<>
 			{!isMenuOpen ? (
-				<header className='absolute left-0 top-0 flex w-full flex-1 justify-between bg-neutral-800/70 px-6 py-3'>
+				<header className={`fixed ${isHeaderSticky ? ('bg-neutral-800/50') : ('bg-neutral-800/80')} transition duration-300 z-10 top-0 flex w-full justify-between px-6 py-3`}>
 					<Image
 						src={logo}
 						alt='Logo da Domingos Zion'
@@ -36,7 +51,7 @@ function Header() {
 					</button>
 				</header>
 			) : (
-				<header className='fixed left-0 right-0 top-0 z-50 flex h-full flex-col gap-20 bg-neutral-800 px-6 py-3'>
+				<header className='fixed left-0 right-0 top-0 z-50 flex h-full flex-col gap-20 bg-neutral-950 px-6 py-3'>
 					<div className='flex justify-between'>
 						<Image
 							src={logo}
