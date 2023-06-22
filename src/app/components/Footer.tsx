d1c864fafb620743161e7b7f520e2ccb47bb185a
Gutinnho <glunardi99@gmail.com>
2023-06-22 14:30:45 -0300

    feat(footer): Added responsiveness, credits, and effects
    Added responsiveness for improved viewing on larger screen sizes
    Added credits for the Art and Design Icons used in the project
    Added color-changing effect and levitation effect

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,55 +1,67 @@
-import Image from 'next/image';
-
-import logo from '@/assets/Logo.png';
 import InstagramIconSVG from './SVGs/InstagramIconSVG';
 import FacebookIconSVG from './SVGs/FacebookIconSVG';
 import WhatsappIcon from './SVGs/WhatsappIconSVG';
+import Logo from '@components/Logo';
 
 function Footer() {
 	return (
-		<footer className=' flex flex-col gap-3 bg-neutral-800 px-6 py-3'>
+		<footer className=' flex flex-col gap-3 bg-neutral-800 px-6 py-3 md:px-16 md:py-6 lg:gap-5 lg:px-20 lg:py-8'>
 			<div className='flex justify-between'>
-				<div className='flex items-center'>
-					<Image
-						src={logo}
-						alt='Logo da Domingos Zion'
-						width={48}
-						height={48}
-						quality={100}
-					/>
+				<div className='self-center'>
+					<Logo />
 				</div>
 
 				<div className='flex justify-center gap-2'>
 					<ul className='flex flex-col items-end justify-center gap-1'>
-						<li className='text-xs font-bold uppercase text-white'>Início</li>
-						<li className='text-xs font-bold uppercase text-white'>Galeria</li>
-						<li className='text-xs font-bold uppercase text-white'>Contato</li>
+						<li className='levitation text-xs font-bold uppercase text-white md:text-sm lg:text-base'>
+							<a href='#'>Início</a>
+						</li>
+						<li className='levitation text-xs font-bold uppercase text-white md:text-sm lg:text-base'>
+							<a href='#'>Galeria</a>
+						</li>
+						<li className='levitation text-xs font-bold uppercase text-white md:text-sm lg:text-base'>
+							<a href='#'>Contato</a>
+						</li>
 					</ul>
 
-					<div className='flex flex-col gap-2'>
-						<button className='border-2 border-blue-900 px-1 py-1 text-xs font-bold uppercase text-white drop-shadow-custom'>
+					<div className='flex flex-col gap-2 md:gap-4'>
+						<button className='border-2 border-blue-900 px-1 py-1 text-xs font-bold uppercase text-white drop-shadow-custom transition duration-300 hover:bg-blue-900 md:text-sm'>
 							Agende Agora
 						</button>
 
-						<div className='flex justify-center gap-2'>
+						<div className='flex justify-center gap-2 md:gap-4'>
 							<a href='#'>
-								<InstagramIconSVG fill='#2B488A' size={24} />
+								<InstagramIconSVG fill='#2B488A' />
 							</a>
 
 							<a href='#'>
-								<FacebookIconSVG fill='#2B488A' size={24} />
+								<FacebookIconSVG fill='#2B488A' />
 							</a>
 
 							<a href='#'>
-								<WhatsappIcon fill='#2B488A' size={24} />
+								<WhatsappIcon fill='#2B488A' />
 							</a>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div className='flex justify-center border-t border-blue-900 px-3 py-1'>
-				<p className='text-xs font-bold text-white'>
-					&copy; 2023 Gustavo Lunardi. Todos os direitos reservados.
+			<div className='flex flex-col justify-start border-t border-blue-900 px-3 py-1 md:px-6 md:py-3'>
+				<p className='text-xs font-bold text-white md:text-sm'>
+					&copy; {new Date().getFullYear()}{' '}
+					<a href='https://github.com/Gutinnho' className='hover:brightness-75'>
+						Gustavo Lunardi
+					</a>
+					. Todos os direitos reservados.
+				</p>
+				<p
+					title='art and design icons'
+					className='text-xs font-bold text-white md:text-sm'>
+					Art and design icons created by{' '}
+					<a
+						href='https://www.flaticon.com/free-icons/art-and-design'
+						className='hover:brightness-75'>
+						Freepik - Flaticon
+					</a>
 				</p>
 			</div>
 		</footer>
